aaa144bd6f1733439c350ee2fc33fd3bc4924546
wuliguaiguaia <1944063509@qq.com>
2018-10-21 15:30:03 +0800

    :tada:

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /*TODO
-* 1.改变窗口画布内容不变
-* 2.自定义图形
-* 3.自定义线宽
-*/ 
+ * 1.改变窗口画布内容不变
+ * 2.自定义图形
+ * 3.自定义线宽
+ */
 
 
 // 1.初始化，创建颜色
 init();
-// 2.画布大小
 let cxt = canvas.getContext('2d');
+// canvas的偏移位置
 let diff = {
     x: 5,
     y: canvas.offsetTop
@@ -18,50 +18,39 @@ let diff = {
 listenUser();
 
 let paintFlag = true,
-    using = false;
-let x1, x2, y1, y2, color;
+    color,
+    lineWidth = lineConfig.value,
+    eraserRect = wipeConfig.value;
 
-canvas.onmousedown = function (e) {
-    using = true;
-    x1 = e.clientX - diff.x;
-    y1 = e.clientY - diff.y;
-    cxt.clearRect(x1, y1, 10, 10)
-    canvas.onmousemove = function (e) {
-        x2 = e.clientX - diff.x;
-        y2 = e.clientY - diff.y;
-        if (using) {
-            if (paintFlag) {
-                drawLine(cxt, x1, y1, x2, y2, color)
-                x1 = x2;
-                y1 = y2;
 
-            } else {
-                cxt.clearRect(x2, y2, 10, 10)
-            }
-        }
-    }
-    canvas.onmouseup = function (e) {
-        using = false;
-    }
-}
 
 colorBox.onclick = function (e) {
-    if (e.target != colorBox) {
-        color = e.target.dataColor;
+    if (paintFlag) {
+        if (e.target != colorBox) {
+            color = e.target.dataColor;
+            pen.style.color = color == "#fff" ? "#ccc" : color;
+        }
     }
 }
 
-button.onclick = function () {
-    if (this.classList.contains("active")) {
+button.onclick = function (e) {
 
-        this.classList.remove("active");
-        paintFlag = false;
-    } else {
-        this.classList.add("active");
+    e.target.classList.add("active");
+    if (e.target == pen) {
         paintFlag = true;
+        eraser.classList.remove("active");
+    } else {
+        paintFlag = false;
+        pen.classList.remove("active");
     }
 }
 
+lineConfig.oninput = function (e) {
+    lineWidth = e.target.value;
+}
+wipeConfig.oninput = function (e) {
+    eraserRect = e.target.value;
+}
 
 
 
@@ -73,19 +62,20 @@ function init() {
 
 function listenUser() {
     window.onresize = resize;
+    canvasDrawing();
 }
 
-function drawLine(cxt, x1, y1, x2, y2, color) {
+function drawLine(cxt, x1, y1, x2, y2, color, lineWidth) {
     cxt.beginPath();
     cxt.moveTo(x1, y1);
     cxt.lineTo(x2, y2);
-    cxt.lineWidth = 4;
+    cxt.lineWidth = lineWidth;
     cxt.strokeStyle = color;
     cxt.stroke();
 }
 
 function createColor() {
-    let colorHash = ['#000', "#7f7f7f", "#880015", "#1d1c24", "#ff2f27", "#fff200", "#22b14c", "#00a2e8",
+    let colorHash = ['#000', "#7f7f7f", "#880015", "#ff2f27", "#fff200", "#22b14c", "#00a2e8",
         "#3f48cc",
         "#a349a4", "#fff", "#c3c3c3", "#b97a57", "#ffaec9"
     ];
@@ -101,8 +91,46 @@ function createColor() {
 }
 
 function resize() {
-    let W = getComputedStyle(canvas.parentNode, null).width;
+    let W = canvas.parentNode.offsetWidth;
     let H = window.innerHeight - canvas.offsetTop - 10;
     canvas.width = parseInt(W);
     canvas.height = parseInt(H);
+}
+
+function canvasDrawing() {
+    let x1, x2, y1, y2, using = false;
+    canvas.onmousedown = start
+    canvas.onmousemove = move
+    canvas.onmouseup = end
+    canvas.ontouchstart = start
+    canvas.ontouchmove = move
+    canvas.ontouchend = end;
+
+    function start(e) {
+        if (e.touches) e = e.touches[0];
+        using = true;
+        x1 = e.clientX - diff.x;
+        y1 = e.clientY - diff.y;
+        cxt.clearRect(x1, y1, eraserRect, eraserRect)
+    }
+
+    function move(e) {
+        if (e.touches) e = e.touches[0]
+        x2 = e.clientX - diff.x;
+        y2 = e.clientY - diff.y;
+        if (using) {
+            if (paintFlag) {
+                drawLine(cxt, x1, y1, x2, y2, color, lineWidth)
+                x1 = x2;
+                y1 = y2;
+
+            } else {
+                cxt.clearRect(x2, y2, eraserRect, eraserRect)
+            }
+        }
+    }
+
+    function end() {
+        using = false;
+    }
 }
